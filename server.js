f0d02499698966f06759ae44b17889e4bd8d8776
= <=>
2026-04-19 11:22:55 +0500

    added more pages in public dir

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+// Access Public folders
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/about',(req,res) =>{
-    res.send("This is about page");
+   // res.send("This is about page");
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+
 })
 
 app.get('/contact',(req,res) =>{
