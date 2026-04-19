f6840be0690aa7e0d64a55d8bbc55ac4f7ff9c80
= <=>
2026-04-19 11:05:37 +0500

    added about page

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/about',(req,res) =>{
+    res.send("This is about page");
+})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
